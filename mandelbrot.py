17a4e2aea18e55cac4fd7c8ebd17895e8844d43f
Isacco Faglioni <isaccofaglioni2000@gmail.com>
2023-06-19 18:59:35 +0200

    working version of the program, there is still a long way to go but this is the first working one!

--- mandelbrot.py
+++ mandelbrot.py
@@ -1,13 +1,65 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+#since the condition on the mandelbrot fractal is indipendent of the class I can write it outside
+
+
+def next_f(z0: complex, c: complex):
+    """
+    Functions that computes the netx element in the recursive formula:
+    zn+1 = zn*zn + c
+    z0: should almost always be initialized to 0
+    c   is a comlex constant
+    """
+    #NOTE for isacco: maybe this can be implemented via lambda
+    z1 = z0**2 + c
+    return z1
+
+def n_iter(z0: complex, c: complex, max_iter: int, thresh = 4):
+    """
+    Returns the number of iterations before z diverges (given a treshold)
+    """
+    zn = z0
+    it = 0
+    while ((abs(zn)**2 < thresh) and it < max_iter):
+        zn = next_f(zn, c)
+        it += 1
+    return it
+        
 class fractal_board:
     """
     board to rapresent the Mandelbrot set
     """
     def __init__(self, dim):
-        self.board = np.full((dim, dim), 256)
+        self.dim = dim
+        self.board = np.full((self.dim, self.dim), 256)
 
-a = fractal_board(1000)
+    """
+    keep in mind that The left-most extent of the set ends with the spike 
+    at x = -2, and the right side extends out to approximately x = 0.47. 
+    The top and bottom are at approximately y = ± 1.12
+    """
+    def coloring_board(self):
+        """
+        function that colors each pixel the board basing on number of iters
+        """
+        z_real_bound = 2.48/(self.dim-1)
+        z_im_bound = 2.26/(self.dim-1)
+        #lamda func that maps the coordinates into the rescaled ones
+        f = lambda z_real, z_im:(z_real*z_real_bound - 2, z_im*z_im_bound - 1.13)
+        for x in range(self.dim):
+            for y in range(self.dim):
+                number_iterations = n_iter(0, complex(*f(x,y)), 500)
+                self.board[y][x] = 255-number_iterations
 
-print(a.board)
+    def plotter(self):
+        """
+        shows the fractal
+        """
+        plt.imshow(self.board, cmap = "plasma")
+        plt.axis('off')
+        plt.show()
+
+a =  fractal_board(1000)
+a.coloring_board()
+a.plotter()
