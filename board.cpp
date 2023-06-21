8c3a9d7a7df1c0ae2c9d86fd16240559e3dfdd3b
Isacco Faglioni <isaccofaglioni2000@gmail.com>
2023-06-21 21:43:37 +0200

    firts 'full' implementation, probably still not working properly

--- board.cpp
+++ board.cpp
@@ -1,50 +1,82 @@
-#include<vector>
-#include<iostream>
-//std::vector<std::vector<int>> isi(rows, std::vector<int>(cols, 256));
+#include <iostream>
+#include <vector>
+// std::vector<std::vector<int>> isi(rows, std::vector<int>(cols, 256));
 
-struct complex
-{
-  int r;
-  int i;
+struct complex {
+  double r;
+  double i;
 };
 
-class BOARD
-{
+  double mod_2(complex a)
+  {
+    return (a.r*a.r + a.i*a.i);
+  };
+
+class BOARD {
 private:
   int m_dim;
   std::vector<int> m_board;
+
 public:
-  BOARD(int dim): m_dim{dim}, m_board(m_dim*m_dim,256)
+  BOARD(int dim) : m_dim{dim}, m_board(m_dim * m_dim, 256){};
+
+  void print_board() {
+    for (int i = 0; i < m_dim * m_dim; ++i)
+      {
+      std::cout << m_board[i] << ' ';
+      if (i % (m_dim - 1) == 0) {
+        std::cout << '\n';
+      }
+    }
+  };
+  complex get_coordinates(int n) {
+    complex c;
+    //type conversions are ugly, but int division is efficient
+    int _i = c.i;
+    int _r = c.r;
+    _i = n / m_dim;
+    _r = n % m_dim;
+    double i_ = _i;
+    double r_ = _r;
+    c.i = (i_/m_dim)*2.26 - 1.13;
+    c.r = (r_/m_dim)*2.48 - 2;
+    return c;
+  };
+  void test() {
+    for (int i = 0; i < 100; ++i) {
+      std::cout << get_coordinates(i).i << ' ' << get_coordinates(i).r << '\n';
+    }
+  };
+  complex next_s(complex z0) 
+    // this function calculates the number of iterations for each pixel
   {
+    //maybe implement mod in struct
+    //use the recursive formula:
+    complex z;
+    complex c;
+    c.i = 0;
+    c.r = 0;
+    z.i = 2*z0.i*z0.r + c.i;
+    z.r = z.r*z.r - z.i*z.i + c.r;
+    return z;
   };
 
-void print_board()
-{
-  for (int i=0; i<m_dim*m_dim; ++i)
-    {
-      std::cout<< m_board[i] << ' ';
-      if (i%(m_dim-1) == 0)
-	{
-	  std::cout<<'\n';
-	}
-    }
-};
-  complex get_coordinates(int n)
+  int num_iter(complex z, int max_iter)
   {
-    complex c;
-    int row;
-    c.i = n/m_dim;
-    c.r = n%m_dim;
-    return c;
-      };
-  void get_coordinates_test(){
-    for (int i = 0; i<100; ++i)
+    int iter = 0;
+    while (iter<max_iter && mod_2(z)<4)
       {
-	std::cout<< get_coordinates(i).i<< ' '<< get_coordinates(i).r<<'\n';
+	z = next_s(z);
+	++iter;
+      }
+    return iter;
+  };
+
+  void board_iter()
+  {
+    for (int i=0; i<m_dim*m_dim; ++i)
+      {
+	std::cout<< num_iter(get_coordinates(i), 200) << '\n';
       }
   };
-  
-    
 };
-
-
--- main.cpp
+++ main.cpp
@@ -1,21 +1,24 @@
 //this isn't a true main, I just need to have fast calculations to pass to np ar
 #include<iostream>
 #include<vector>
-
-
+#include<complex>
+#include"board.cpp"
+/*
+std::complex<double> next_f(std::complex<double> c)
+{
+  //function that computes the next fractal with the recursive formula:
+  //zn+1 = zn*zn + c
+  //  std::complex<double> z1(0,0);
+  std::complex<double> z0(1,1);
+  std::complex<double> z1 = real(z0)*real(z0) - imag(z0)*imag(z0) + 2*real(z0)*imag(z0) + c;
+  return z1;
+};
+*/
 
 int main()
 {
-  int rows = 3;
-  int cols = 3;
-  std::vector<std::vector<int>> isi(rows, std::vector<int>(cols, 256));  
-  for (int i = 0; i<rows; ++i)
-    for (int j = 0; j <cols; ++j)
-      {
-	{
-	  std::cout<< isi[i][j];
-	}
-      }
+  BOARD a(1000);
+  a.print_board();
+  a.board_iter();
   return 0;
-  
 }
